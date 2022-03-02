e50b8a639d61cd9b0eb1ea92b753d0b5d30b02a0
hirutu <gyoyo@gmail.com>
2022-03-02 11:43:05 +0700

    upload thu muc twct vo github

---
+++ twct/drm/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class drm(model.models):
+       tengoi=models.CharField(max_length=400)
+       chucnang=models.CharField(max_length=300)
+       congdung=models.CharField(max_length=400)
+       sd=models.CharField(max_length=300) 
+        
--- twct/twct/settings.py
+++ twct/twct/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django-heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,10 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'http://www.tnln12.herokuapp.com/static/'
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
