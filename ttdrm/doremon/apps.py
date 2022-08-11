e2183abc307e1ed7632334ee14187366cc65eb04
jiyop <jiji333@gmail.com>
2022-08-11 13:03:59 +0700

    upload thu muc ttdrm vo github

---
+++ ttdrm/doremon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DoremonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'doremon'
---
+++ ttdrm/doremon/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+class drm(models.Model):
+       tennv = models.CharField(max_length=800)
+       theloai = models.CharField(max_length=1200)
+       congnang = models.CharField(max_length=1500)
+def __str__(self):
+        return self.tinnhan 
+
